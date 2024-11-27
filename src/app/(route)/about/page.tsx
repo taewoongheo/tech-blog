0404ed5dd5cbc7266633ed8d48e4eede48e38cee
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-11-27 20:05:48 +0900

    feat(Post): on-demand post path 들을 static 하도록 변경. generateStaticParams()

--- src/app/(route)/about/page.tsx
+++ src/app/(route)/about/page.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-
-const About = (): React.ReactElement => {
-  return <h1>about</h1>;
-};
-
-export default About;
+export default function About() {
+  return <h1>About</h1>;
+}
