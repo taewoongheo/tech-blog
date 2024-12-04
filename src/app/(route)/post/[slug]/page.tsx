798247c9b7d9552516fae71cc9a18dae97acc00d
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-12-04 18:27:41 +0900

    feat(Post): build time 에 metadata 설정

--- src/app/(route)/post/[slug]/page.tsx
+++ src/app/(route)/post/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import getPostPaths from '@/app/_utils/getPostPaths';
 import path from 'path';
-import fs from 'fs';
 import { promises as pfs } from 'fs';
 import { glob } from 'glob';
 import matter from 'gray-matter';
 import { compile, run } from '@mdx-js/mdx';
 import * as runtime from 'react/jsx-runtime';
+import { Metadata } from 'next';
 
 type Props = {
   params: { slug: string };
@@ -14,14 +14,19 @@ type Props = {
 /**
  * build 시점에 실행할 slug 반환
  */
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<Props[]> {
   const paths = await getPostPaths();
   return paths.map((filePath) => ({
     params: { slug: path.basename(filePath, '.mdx') },
   }));
 }
 
-export default async function Post({ params }: Props) {
+/**
+ * build 시점에 메타데이터 생성
+ *
+ * TODO: metadataBase 설정: OG, Canonical URL
+ */
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { slug } = await params;
   const pathStr = path.join(
     process.cwd(),
@@ -31,8 +36,58 @@ export default async function Post({ params }: Props) {
     `**/${slug}.mdx`,
   );
   const postPath = (await glob(pathStr))[0];
-  if (!fs.existsSync(postPath)) {
-    return <h1>Post not found</h1>;
+
+  if (!pfs.access(postPath)) {
+    return {
+      title: 'post not found',
+      description: 'post not fonud',
+      referrer: 'origin-when-cross-origin',
+      generator: 'Next.js',
+      applicationName: 'TaewoongHeo Tech Blog',
+      keywords: [],
+      formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+      },
+    };
+  }
+
+  const data = await pfs.readFile(postPath, 'utf-8');
+  const metadata = matter(data).data;
+
+  return {
+    generator: 'Next.js',
+    applicationName: 'TaewoongHeo Tech Blog',
+    referrer: 'origin-when-cross-origin',
+    title: metadata.title,
+    description: metadata.description,
+    keywords: metadata.tags,
+    formatDetection: {
+      email: false,
+      address: false,
+      telephone: false,
+    },
+  };
+}
+
+/**
+ * TODO: slug 가 겹치지 않도록 보장해야함.
+ */
+export default async function Post({
+  params,
+}: Props): Promise<React.ReactNode> {
+  const { slug } = await params;
+  const pathStr = path.join(
+    process.cwd(),
+    'src',
+    'app',
+    '_posts',
+    `**/${slug}.mdx`,
+  );
+  const postPath = (await glob(pathStr))[0];
+  if (!pfs.access(postPath)) {
+    return <h1>not found</h1>;
   }
   const data = await pfs.readFile(postPath, 'utf-8');
   const content = matter(data).content;
