d6268b822120daf0451d416a2c73f2ea8c5318bf
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-12-21 16:36:48 +0900

    chore(eslint): react hook 규칙 수정
    react compoennt 가 아닌 component 에서도 hook 을 사용할 수 있도록 규칙변경
    useMDXComponents() 로 커스텀한 html 을 적용하기 위함

--- src/app/(route)/post/[slug]/layout.tsx
+++ src/app/(route)/post/[slug]/layout.tsx
@@ -1,4 +1,10 @@
-export default function MdxLayout({ children }: { children: React.ReactNode }) {
+import React from 'react';
+
+export default function MdxLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): React.ReactNode {
   return (
     <div className="flex flex-col mt-10 sm:mt-md mb-10 px-all sm:px-sm md:px-md lg:px-lg xl:px-xl">
       {children}
