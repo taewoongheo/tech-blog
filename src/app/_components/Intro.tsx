2557995bd6d6f865e914ad9c7c58d884fd1eb37f
taewoongheo <chamjoeun0111@gmail.com>
2025-03-02 20:51:13 +0900

    "await은 어떻게 동기적으로 동작할까? 코드 뜯어보며 살펴보기" 글 작성

--- src/app/_components/Intro.tsx
+++ src/app/_components/Intro.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TypingIntro from './TypingIntro';
 
 export default function Intro(): React.ReactNode {
   return (
@@ -25,12 +26,7 @@ export default function Intro(): React.ReactNode {
           <h1 className="text-main font-main text-[2rem] md:text-[2.2rem] font-[600] lg:mb-2">
             허태웅 기술 블로그
           </h1>
-          <p className="text-main font-main opacity-70 text-[1rem] md:text-[1.2rem] font-[500]">
-            배우고 고민한 흔적들을 공유합니다
-          </p>
-          <p className="text-main font-main opacity-70 text-[1rem] md:text-[1.2rem] font-[500]">
-            깊게 고민하고 쉽게 설명하려 노력해요
-          </p>
+          <TypingIntro />
         </div>
       </div>
     </header>
---
+++ src/app/_components/TypingIntro.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TypingIntro() {
+  return (
+    <>
+      <p className="text-main font-main opacity-70 text-[1rem] md:text-[1.2rem] font-[500]">
+        배우고 고민한 흔적들을 공유합니다
+      </p>
+      <p className="text-main font-main opacity-70 text-[1rem] md:text-[1.2rem] font-[500]">
+        깊게 고민하고 쉽게 설명하려 노력해요
+      </p>
+    </>
+  );
+}
+
+export default TypingIntro;
